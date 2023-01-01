3aedec8a1e93e4252b726e1605709abd515897cd
Jainy Jain <zikzakjainy@gmail.com>
2023-01-01 13:49:41 +0530

    build issue fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// import {Configuration, OpenAIApi} from 'openai';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularchatgpt';
-  // config = new Configuration({
-  //   apiKey: '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'
-  // })
-  // openAi = new OpenAIApi(this.config);
-  
-  constructor() {
-  //  this.myGPT()
-  }
-
-//  async myGPT() {
-//   const response = await this.openAi.createCompletion({
-//     model: 'text-davinci-003',
-//     prompt: 'What is discharge summary?',
-//     max_tokens: 3000,
-//     temperature: 0,
-//     top_p: 1,
-//     frequency_penalty: 0.5,
-//     presence_penalty: 0
-//   });
-//   const chatGptResponse = response.data.choices[0].text;
-//   console.log('chatGptResponse ', chatGptResponse);
-//  }
+  constructor() {}
 }
